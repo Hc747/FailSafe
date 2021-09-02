be321a9311f9d6aac35d19b312a365f1109b1bc9
Harrison Cole <harrison.cole@mnfgroup.limited>
2021-09-02 10:27:23 +1000

    - add EasingFunction method overloads using TimeUnit API.

--- src/main/java/com/github/hc747/failsafe/retry/backoff/EasingFunction.java
+++ src/main/java/com/github/hc747/failsafe/retry/backoff/EasingFunction.java
@@ -3,6 +3,7 @@ package com.github.hc747.failsafe.retry.backoff;
 import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 @FunctionalInterface
 public interface EasingFunction {
@@ -10,19 +11,36 @@ public interface EasingFunction {
     long ease(int attempts);
 
     default EasingFunction plus(EasingFunction function) {
+        Objects.requireNonNull(function, "function");
         return (attempts) -> ease(attempts) + function.ease(attempts);
     }
 
     default EasingFunction minus(EasingFunction function) {
+        Objects.requireNonNull(function, "function");
         return (attempts) -> ease(attempts) - function.ease(attempts);
+    }
+
+    default EasingFunction limit(long duration, TimeUnit unit) {
+        Objects.requireNonNull(unit, "unit");
+        return limit(unit.toMillis(duration));
     }
 
     default EasingFunction limit(long limit) {
         return (attempts) -> Math.min(limit, ease(attempts));
     }
 
+    static EasingFunction fixed(long duration, TimeUnit unit) {
+        Objects.requireNonNull(unit, "unit");
+        return fixed(unit.toMillis(duration));
+    }
+
     static EasingFunction fixed(long delay) {
         return (attempts) -> delay;
+    }
+
+    static EasingFunction linear(long duration, TimeUnit unit) {
+        Objects.requireNonNull(unit, "unit");
+        return linear(unit.toMillis(duration));
     }
 
     static EasingFunction linear(long multiplier) {
@@ -33,8 +51,18 @@ public interface EasingFunction {
         return (attempts) -> Math.round(Math.pow(attempts, 2));
     }
 
+    static EasingFunction random(long min, long max, TimeUnit unit) {
+        Objects.requireNonNull(unit, "unit");
+        return random(unit.toMillis(min), unit.toMillis(max));
+    }
+
     static EasingFunction random(long min, long max) {
         return (attempts) -> (long) (ThreadLocalRandom.current().nextDouble() * (max - min)) + min;
+    }
+
+    static EasingFunction random(Random random, long min, long max, TimeUnit unit) {
+        Objects.requireNonNull(unit, "unit");
+        return random(random, unit.toMillis(min), unit.toMillis(max));
     }
 
     static EasingFunction random(Random random, long min, long max) {
